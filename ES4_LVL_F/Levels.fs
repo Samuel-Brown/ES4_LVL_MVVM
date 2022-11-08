cb62da9826ab422e49c69f27f803059fde9c7d40
Sam Brown <sam.brownie1@gmail.com>
2022-11-08 05:10:40 +0000

    Added levels

--- ES4_LVL_F/Levels.fs
+++ ES4_LVL_F/Levels.fs
@@ -2,36 +2,40 @@
 
 type Level =
     { 
-    Armorer        : int
-    Athletics      : int
-    Blade          : int
-    Block          : int
-    Blunt          : int
-    Hand_to_Hand   : int
-    Heavy_Armor    : int
-    Alchemy        : int
-    Alteration     : int
-    Conjuration    : int
-    Destruction    : int
-    Illusion       : int
-    Mysticism      : int
-    Restoration    : int
-    Acrobatics     : int
-    Light_Armor    : int
-    Marksman       : int
-    Mercantile     : int
-    Security       : int
-    Sneak          : int
-    Speechcraft    : int
+    Armorer_Increased_By        : int
+    Athletics_Increased_By      : int
+    Blade_Increased_By          : int
+    Block_Increased_By          : int
+    Blunt_Increased_By          : int
+    Hand_to_Hand_Increased_By   : int
+    Heavy_Armor_Increased_By    : int
+    Alchemy_Increased_By        : int
+    Alteration_Increased_By     : int
+    Conjuration_Increased_By    : int
+    Destruction_Increased_By    : int
+    Illusion_Increased_By       : int
+    Mysticism_Increased_By      : int
+    Restoration_Increased_By    : int
+    Acrobatics_Increased_By     : int
+    Light_Armor_Increased_By    : int
+    Marksman_Increased_By       : int
+    Mercantile_Increased_By     : int
+    Security_Increased_By       : int
+    Sneak_Increased_By          : int
+    Speechcraft_Increased_By    : int
 
-    Strength       : int
-    Intelligence   : int
-    Willpower      : int
-    Agility        : int
-    Speed          : int
-    Endurance      : int
-    Personality    : int
-    Luck           : int
+    Strength_Add       : int
+    Intelligence_Add   : int
+    Willpower_Add      : int
+    Agility_Add        : int
+    Speed_Add          : int
+    Endurance_Add      : int
+    Personality_Add    : int
+    Luck_Add           : int
+
+
+
+
 
     }
 
